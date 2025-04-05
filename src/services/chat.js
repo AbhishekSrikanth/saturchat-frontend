7364a55d0da200c8c926be9c12cc00114a915ba5
Abhishek Srikanth <abhisrik99@outlook.com>
2025-04-05 17:24:57 -0400

    feat(chat): add user search and conversation creation functions

--- src/services/chat.js
+++ src/services/chat.js
@@ -1,6 +1,16 @@
 import api from './axios';
 
 export async function getConversations() {
-  const res = await api.get('/api/chat/conversations/');
-  return res.data;
+    const res = await api.get('/api/chat/conversations/');
+    return res.data;
+}
+
+export async function searchUsers(query) {
+    const res = await api.get(`/api/users/search/?q=${query}`);
+    return res.data;
+}
+
+export async function createConversation(data) {
+    const res = await api.post('/api/chat/conversations/', data);
+    return res.data;
 }
