36176b1f322d0cf2f1bda1e50c44a407bc760417
Abhishek Srikanth <abhisrik99@outlook.com>
2025-04-05 16:17:48 -0400

    fix(auth): update registerUser function to accept password and confirmed password fields

--- src/services/auth.js
+++ src/services/auth.js
@@ -5,7 +5,12 @@ export async function loginUser({ username, password }) {
   return res.data;
 }
 
-export async function registerUser({ username, password }) {
-  const res = await api.post('/api/auth/register/', { username, password });
+export async function registerUser({ username, password1, password2 }) {
+  const res = await api.post('/api/auth/register/', {
+    username,
+    password1,
+    password2,
+  });
   return res.data;
 }
+
