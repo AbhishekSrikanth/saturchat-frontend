35f3dc36194ce115c82d4eb2b7a06e77b83bdce0
Abhishek Srikanth <abhisrik99@outlook.com>
2025-04-05 17:05:04 -0400

    feat(main): import icons module for enhanced user interface

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './icons';
 import { AuthProvider } from './context/AuthProvider'
 
 createRoot(document.getElementById('root')).render(
