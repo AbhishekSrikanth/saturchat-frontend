2be92ff9947ae4f2666ea13287804565c3080107
Abhishek Srikanth <abhisrik99@outlook.com>
2025-04-05 16:00:40 -0400

    fix(auth): update API endpoints for login and registration

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,11 +1,11 @@
 import api from './axios';
 
 export async function loginUser({ username, password }) {
-  const res = await api.post('/api/token/', { username, password });
+  const res = await api.post('/api/auth/login/', { username, password });
   return res.data;
 }
 
 export async function registerUser({ username, password }) {
-  const res = await api.post('/register/', { username, password });
+  const res = await api.post('/api/auth/register/', { username, password });
   return res.data;
 }
