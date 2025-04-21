a3f965ae27b43ce6cbe3779f143fc25561e1a1a0
Abhishek Srikanth <abhisrik99@outlook.com>
2025-04-21 17:47:05 -0400

    feat(vite.config.js): load environment variables for WebSocket base URL

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import process from 'process';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      __WS_BASE_URL__: JSON.stringify(env.VITE_WS_BASE_URL),
+    },
+  };
+});
